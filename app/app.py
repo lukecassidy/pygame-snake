418a557fe15c829331227ed61e9e09962b193676
Luke Cassidy <lukecassidy88@gmail.com>
2025-08-29 16:46:42 +0100

    Centralise our config settings

--- app/app.py
+++ app/app.py
@@ -3,24 +3,28 @@
 A simple snake game using pygame
 """
 
-
 import random
 from enum import Enum
 import pygame
 import sys
 
-
-BLOCK_SIZE = 10
-SCORE_PER_FOOD = 250
-GROWTH_PER_FOOD = 2
-
-
-class Colour:
+class COLORS:
     WHITE = (255, 255, 255)
     BLACK = (0, 0, 0)
-    RED = (255, 0, 0)
+    RED   = (255, 0, 0)
     GREEN = (0, 150, 0)
-    BLUE = (0, 0, 255)
+    BLUE  = (0, 0, 255)
+
+
+class CONFIG:
+    BLOCK_SIZE      = 10
+    SCORE_PER_FOOD  = 250
+    GROWTH_PER_FOOD = 2
+    FPS             = 20
+    SCREEN_WIDTH    = 600
+    SCREEN_HEIGHT   = 400
+    CAPTION         = "Snake"
+    SCORE_COLOUR    = COLORS.RED
 
 
 class Direction(Enum):
@@ -32,11 +36,11 @@ class Direction(Enum):
 
 
 class Game:
-    caption = "Snake"
-    fps = 20
-    screen_width = 600
-    screen_height = 400
-    score_colour = Colour.RED
+    caption = CONFIG.CAPTION
+    fps = CONFIG.FPS
+    screen_width = CONFIG.SCREEN_WIDTH
+    screen_height = CONFIG.SCREEN_HEIGHT
+    score_colour = CONFIG.SCORE_COLOUR
 
     def __init__(self, surface: pygame.Surface):
         self.surface = surface
@@ -45,7 +49,6 @@ class Game:
         self.font_small = pygame.font.SysFont(None, 25)
         self.font_med = pygame.font.SysFont(None, 30)
         self.font_large = pygame.font.SysFont(None, 40)
-
 
     def display_score(self):
         message = f"score: {self.score}"
@@ -70,7 +73,7 @@ class Game:
         )
         self.surface.blit(text_surface, text_rect)
 
-    def get_score(self) -> int:
+    def get_score(self):
         return self.score
 
     def reset(self):
@@ -82,17 +85,16 @@ class Game:
 
 
 class Snake:
-    colour = Colour.GREEN
-    block_size = BLOCK_SIZE
+    colour = COLORS.GREEN
+    block_size = CONFIG.BLOCK_SIZE
 
     def __init__(self):
         self.snake_list = []
-        self.snake_head = []
         self.length = 1               # Increase when you eat food
         self.speed = self.block_size  # Move by one grid cell
         # Start centered on the grid
-        self.head_x = (Game.screen_width // 2 // self.block_size) * self.block_size
-        self.head_y = (Game.screen_height // 2 // self.block_size) * self.block_size
+        self.head_x = (CONFIG.SCREEN_WIDTH // 2 // self.block_size) * self.block_size
+        self.head_y = (CONFIG.SCREEN_HEIGHT // 2 // self.block_size) * self.block_size
         self.x_change = 0
         self.y_change = 0
         self.direction = Direction.NONE
@@ -100,34 +102,32 @@ class Snake:
     def reset(self):
         self.length = 1
         self.snake_list = []
-        self.snake_head = []
-        self.head_x = (Game.screen_width // 2 // self.block_size) * self.block_size
-        self.head_y = (Game.screen_height // 2 // self.block_size) * self.block_size
+        self.head_x = (CONFIG.SCREEN_WIDTH // 2 // self.block_size) * self.block_size
+        self.head_y = (CONFIG.SCREEN_HEIGHT // 2 // self.block_size) * self.block_size
         self.x_change = 0
         self.y_change = 0
         self.direction = Direction.NONE
 
     def draw_snake(self, surface: pygame.Surface):
         for x, y in self.snake_list:
-            pygame.draw.rect(
-                surface, self.colour, [x, y, self.block_size, self.block_size]
-            )
+            pygame.draw.rect(surface, self.colour, [x, y, self.block_size, self.block_size])
 
-    def check_boundaries(self) -> bool:
-        if self.head_x >= Game.screen_width:
+    def check_boundaries(self):
+        if self.head_x >= CONFIG.SCREEN_WIDTH:
             return True
         if self.head_x < 0:
             return True
-        if self.head_y >= Game.screen_height:
+        if self.head_y >= CONFIG.SCREEN_HEIGHT:
             return True
         if self.head_y < 0:
             return True
         return False
 
-    def check_self_collision(self) -> bool:
+    def check_self_collision(self):
         # If the snake hits itself, game over
+        head = [self.head_x, self.head_y]
         for snake_point in self.snake_list[:-1]:
-            if snake_point == self.snake_head:
+            if snake_point == head:
                 return True
         return False
 
@@ -138,9 +138,10 @@ class Snake:
 
 
 class Food:
+    colour = COLORS.RED
+    block_size = CONFIG.BLOCK_SIZE
+
     def __init__(self):
-        self.colour = Colour.RED
-        self.block_size = BLOCK_SIZE
         self.snake_body_list = []
         self.x = 0
         self.y = 0
@@ -148,8 +149,8 @@ class Food:
 
     def generate_food(self):
         while True:
-            random_x = random.randrange(0, Game.screen_width, self.block_size)
-            random_y = random.randrange(0, Game.screen_height, self.block_size)
+            random_x = random.randrange(0, CONFIG.SCREEN_WIDTH, self.block_size)
+            random_y = random.randrange(0, CONFIG.SCREEN_HEIGHT, self.block_size)
             if [random_x, random_y] not in self.snake_body_list:
                 self.x = random_x
                 self.y = random_y
@@ -159,10 +160,10 @@ class Food:
 def game_intro():
     display_intro = True
     while display_intro:
-        game.surface.fill(Colour.BLACK)
-        game.message_to_screen("Snake", Colour.RED, font_size='large', y_change=-100)
-        game.message_to_screen("Press ENTER to Play", Colour.WHITE, font_size='med', y_change=+20)
-        game.message_to_screen("Press ESC to Quit", Colour.WHITE, font_size='med', y_change=+100)
+        game.surface.fill(COLORS.BLACK)
+        game.message_to_screen("Snake", COLORS.RED, font_size='large', y_change=-100)
+        game.message_to_screen("Press ENTER to Play", COLORS.WHITE, font_size='med', y_change=+20)
+        game.message_to_screen("Press ESC to Quit", COLORS.WHITE, font_size='med', y_change=+100)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 game.exit()
@@ -172,17 +173,17 @@ def game_intro():
                 elif event.key == pygame.K_RETURN:
                     display_intro = False
 
-        game.clock.tick(game.fps)
+        game.clock.tick(CONFIG.FPS)
         pygame.display.update()
 
 
 def game_pause():
     display_pause = True
     while display_pause:
-        game.surface.fill(Colour.BLACK)
-        game.message_to_screen("Snake", Colour.RED, font_size='large', y_change=-100)
-        game.message_to_screen("Press ENTER to Continue", Colour.WHITE, font_size='med', y_change=+20)
-        game.message_to_screen("Press ESC to Quit", Colour.WHITE, font_size='med', y_change=+100)
+        game.surface.fill(COLORS.BLACK)
+        game.message_to_screen("Snake", COLORS.RED, font_size='large', y_change=-100)
+        game.message_to_screen("Press ENTER to Continue", COLORS.WHITE, font_size='med', y_change=+20)
+        game.message_to_screen("Press ESC to Quit", COLORS.WHITE, font_size='med', y_change=+100)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 game.exit()
@@ -192,23 +193,24 @@ def game_pause():
                 elif event.key == pygame.K_RETURN:
                     display_pause = False
 
-        game.clock.tick(game.fps)
+        game.clock.tick(CONFIG.FPS)
         pygame.display.update()
 
 
 def game_loop():
+    # TODO: rename, these are currently confusing
     game_quit = False
     game_over = False
 
     while not game_quit:
         while game_over:
-            game.surface.fill(Colour.BLACK)
-            game.message_to_screen("Game Over", Colour.RED, font_size='large', y_change=-100)
+            game.surface.fill(COLORS.BLACK)
+            game.message_to_screen("Game Over", COLORS.RED, font_size='large', y_change=-100)
             game.message_to_screen(
-                f"Score {game.get_score()}", Colour.RED, font_size='large', y_change=-40
+                f"Score {game.get_score()}", COLORS.RED, font_size='large', y_change=-40
             )
-            game.message_to_screen("Press ENTER to play", Colour.WHITE, font_size='med', y_change=+60)
-            game.message_to_screen("Press ESC to Quit", Colour.WHITE, font_size='med', y_change=+100)
+            game.message_to_screen("Press ENTER to play", COLORS.WHITE, font_size='med', y_change=+60)
+            game.message_to_screen("Press ESC to Quit", COLORS.WHITE, font_size='med', y_change=+100)
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_ESCAPE:
@@ -222,7 +224,7 @@ def game_loop():
                     game.exit()
 
             pygame.display.update()
-            game.clock.tick(game.fps)
+            game.clock.tick(CONFIG.FPS)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -256,12 +258,11 @@ def game_loop():
         if snake.check_boundaries():
             game_over = True
 
-        game.surface.fill(Colour.WHITE)
+        game.surface.fill(COLORS.WHITE)
 
-        pygame.draw.rect(game.surface, Colour.RED, [food.x, food.y, food.block_size, food.block_size])
+        pygame.draw.rect(game.surface, COLORS.RED, [food.x, food.y, food.block_size, food.block_size])
 
-        snake.snake_head = [snake.head_x, snake.head_y]
-        snake.snake_list.append(snake.snake_head)
+        snake.snake_list.append([snake.head_x, snake.head_y])
 
         snake.remove_tail()
 
@@ -273,20 +274,21 @@ def game_loop():
         head_rect = pygame.Rect(snake.head_x, snake.head_y, snake.block_size, snake.block_size)
         food_rect = pygame.Rect(food.x, food.y, food.block_size, food.block_size)
         if head_rect.colliderect(food_rect):
-           food.snake_body_list = snake.snake_list
-           food.generate_food()
-           pygame.draw.rect(game.surface, Colour.RED, [food.x, food.y, food.block_size, food.block_size])
-           snake.length += GROWTH_PER_FOOD
-           game.score += SCORE_PER_FOOD
+            food.snake_body_list = snake.snake_list
+            food.generate_food()
+            pygame.draw.rect(game.surface, COLORS.RED, [food.x, food.y, food.block_size, food.block_size])
+            snake.length += CONFIG.GROWTH_PER_FOOD
+            game.score += CONFIG.SCORE_PER_FOOD
+
         game.display_score()
         pygame.display.update()
-        game.clock.tick(game.fps)
+        game.clock.tick(CONFIG.FPS)
 
 
 def main():
     pygame.init()
-    game_display = pygame.display.set_mode((Game.screen_width, Game.screen_height))
-    pygame.display.set_caption(Game.caption)
+    game_display = pygame.display.set_mode((CONFIG.SCREEN_WIDTH, CONFIG.SCREEN_HEIGHT))
+    pygame.display.set_caption(CONFIG.CAPTION)
 
     global game, snake, food
     game = Game(game_display)
